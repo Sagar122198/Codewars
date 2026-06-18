90bbc9f5bc1cbdff4a54750d4a227d5012dd67ee
Sagar kumar <sagar8789297230kumar@gmail.com>
2026-06-18 11:25:18 +0530

    7kyu

---
+++ 7kyu/String.js
@@ -0,0 +1,10 @@
+// Recover toString() here :)
+Boolean.prototype.toString= function(){
+  return this.valueOf() ? "true" : "false";
+}
+Number.prototype.toString= function(){
+  return String(this.valueOf())
+}
+Array.prototype.toString= function(){
+  return  "[ " + this.join(",") + "] "
+}
